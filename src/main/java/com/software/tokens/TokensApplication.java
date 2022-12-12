0ae4e3b7133893f11f43b0ca62680325455826dd
=hrmirzaagha <=hrmirzaagha@gmail.com>
2022-12-12 10:19:26 +0330

    Create Spring Security Project

---
+++ src/main/java/com/software/tokens/TokensApplication.java
@@ -0,0 +1,13 @@
+package com.software.tokens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/software/tokens/TokensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.tokens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
